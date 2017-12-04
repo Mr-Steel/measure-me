8dde79c4ab1a096bb80d8d37484a636705940241
Mr__Steel <bjoern.steel@gmail.com>
2017-12-05 00:06:59 +0100

    - initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # measure-me
+start with: java -javaagent:lib\jamm-0.3.2.jar -jar target\measure-me-1.0.0-SNAPSHOT.jar
---
+++ src/main/java/net/steel/measure/MeasureMap.java
@@ -0,0 +1,11 @@
+package net.steel.measure;
+
+import org.github.jamm.MemoryMeter;
+
+public class MeasureMap {
+
+    public static void main(String... args) {
+        MemoryMeter meter = new MemoryMeter();
+        System.out.println("sdfasf");
+    }
+}

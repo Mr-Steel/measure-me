0edbd974d4bdee3ab4f35efaee6839590cfb9eb4
Mr__Steel <bjoern.steel@gmail.com>
2017-12-05 23:27:04 +0100

    - added UnifiedMap - first measure use case: adding and removing elements from Maps -> how large are the now empty maps

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # measure-me
 start with: java -javaagent:lib\jamm-0.3.2.jar -jar target\measure-me-1.0.0-SNAPSHOT.jar
+or run main class with vm arguments: "-javaagent:lib\jamm-0.3.2.jar"
--- src/main/java/net/steel/measure/MeasureMap.java
+++ src/main/java/net/steel/measure/MeasureMap.java
@@ -1,11 +1,38 @@
 package net.steel.measure;
 
+import org.eclipse.collections.impl.map.mutable.UnifiedMap;
 import org.github.jamm.MemoryMeter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MeasureMap {
 
     public static void main(String... args) {
         MemoryMeter meter = new MemoryMeter();
+        Map<Integer, Integer> hashMap = new HashMap(100);
+        Map<Integer, Integer> unifiedMap = new UnifiedMap(100);
+        for (int i = 0; i < 100; i++) {
+            hashMap.put(i, i+500);
+            unifiedMap.put(i, i+500);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            hashMap.remove(i);
+            unifiedMap.remove(i);
+        }
+        long measureHashMap = meter.measure(hashMap);
+        long measureDeepHashMap = meter.measureDeep(hashMap);
+
+        long measureUnifiedMap = meter.measure(unifiedMap);
+        long measureDeepUnifiedMap = meter.measureDeep(unifiedMap);
+
+        System.out.println("HashMap_norm: " + measureHashMap);
+        System.out.println("HashMap_deep: " + measureDeepHashMap);
+
+        System.out.println("UnifiedMap_norm: " + measureUnifiedMap);
+        System.out.println("UnifiedMap_deep: " + measureDeepUnifiedMap);
+
         System.out.println("sdfasf");
     }
 }

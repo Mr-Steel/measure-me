e533942a3687d0b7cbbae58ef8357adc870d83d8
BjoernS <bjoerns@lucanet.com>
2017-12-07 11:19:49 +0100

    - added more maps and measurements

--- src/main/java/net/steel/measure/MeasureMap.java
+++ src/main/java/net/steel/measure/MeasureMap.java
@@ -1,6 +1,7 @@
 package net.steel.measure;
 
 import org.eclipse.collections.impl.map.mutable.UnifiedMap;
+import org.eclipse.collections.impl.map.mutable.primitive.ByteObjectHashMap;
 import org.github.jamm.MemoryMeter;
 
 import java.util.HashMap;
@@ -8,31 +9,62 @@ import java.util.Map;
 
 public class MeasureMap {
 
+    private static int capacity = 10;
+
+    private static int multi = 1;
+
     public static void main(String... args) {
         MemoryMeter meter = new MemoryMeter();
-        Map<Integer, Integer> hashMap = new HashMap(100);
-        Map<Integer, Integer> unifiedMap = new UnifiedMap(100);
-        for (int i = 0; i < 100; i++) {
-            hashMap.put(i, i+500);
-            unifiedMap.put(i, i+500);
+        Map<Integer, Integer> hashMap = new HashMap(capacity);
+        Map<Integer, Integer> unifiedMap = new UnifiedMap(capacity);
+        Map<Byte, Integer> hashMapByte = new HashMap<>(capacity);
+        Map<Byte, Integer> unifiedMapByte = new UnifiedMap(capacity);
+        ByteObjectHashMap<Integer> byteObjectHashMap = new ByteObjectHashMap(capacity);
+        for (int i = 0; i < capacity; i++) {
+            hashMap.put(i*multi, i+500);
+            unifiedMap.put(i*multi, i+500);
+            hashMapByte.put((byte)i, i+500);
+            unifiedMapByte.put((byte) i, i+500);
+            byteObjectHashMap.put((byte) i, i+500);
         }
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < capacity; i++) {
             hashMap.remove(i);
             unifiedMap.remove(i);
+            hashMapByte.remove((byte)i);
+            unifiedMapByte.remove((byte)i);
+            byteObjectHashMap.remove((byte)i);
         }
+
+
         long measureHashMap = meter.measure(hashMap);
         long measureDeepHashMap = meter.measureDeep(hashMap);
 
         long measureUnifiedMap = meter.measure(unifiedMap);
         long measureDeepUnifiedMap = meter.measureDeep(unifiedMap);
 
+        long measureHashMapByte = meter.measure(hashMapByte);
+        long measureDeepHashMapByte = meter.measureDeep(hashMapByte);
+
+        long measureUnifiedMapByte = meter.measure(unifiedMapByte);
+        long measureDeepUnifiedMapByte = meter.measureDeep(unifiedMapByte);
+
+        long measureByteObjectHashmap = meter.measure(byteObjectHashMap);
+        long measureDeepByteObjectHashmap = meter.measureDeep(byteObjectHashMap);
+
         System.out.println("HashMap_norm: " + measureHashMap);
         System.out.println("HashMap_deep: " + measureDeepHashMap);
 
-        System.out.println("UnifiedMap_norm: " + measureUnifiedMap);
-        System.out.println("UnifiedMap_deep: " + measureDeepUnifiedMap);
+        System.out.println("Unified_norm: " + measureUnifiedMap);
+        System.out.println("Unified_deep: " + measureDeepUnifiedMap);
 
-        System.out.println("sdfasf");
+        System.out.println("HashMap_Byte_norm: " + measureHashMapByte);
+        System.out.println("HashMap_Byte_deep: " + measureDeepHashMapByte);
+
+        System.out.println("Unified_Byte_norm: " + measureUnifiedMapByte);
+        System.out.println("Unified_Byte_deep: " + measureDeepUnifiedMapByte);
+
+        System.out.println("ByteObjectHashMap_norm: " + measureByteObjectHashmap);
+        System.out.println("ByteObjectHashMap_deep: " + measureDeepByteObjectHashmap);
     }
 }
